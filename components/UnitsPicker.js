74073fe3d1e17a7fe7ae82d60a17c7799f5ceba9
py3thon <76783636+py3thon@users.noreply.github.com>
2021-05-07 15:03:29 +0800

    3 must functions and dark mode
    by harry and nicholas

--- components/UnitsPicker.js
+++ components/UnitsPicker.js
@@ -1,22 +1,20 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
-import{ Picker } from '@react-native-community/picker'
-export default function UnitsPicker({unitSystem,setUnitSystem}) {
+import { Picker } from '@react-native-community/picker'
+export default function UnitsPicker({ unitSystem, setUnitSystem }) {
     return (
-        <View style={styles.unitSystem}>
-            <Picker selectedValue={unitSystem} onValueChange={(item)=> setUnitSystem(item)}>
-                <Picker.Item label="C°" value="metric"/>
-                <Picker.Item label="F°" value="imperial"/>
+        <View style={styles.unit}>
+            <Picker style={{ height: 25, width: 75 }} selectedValue={unitSystem} onValueChange={(item) => setUnitSystem(item)}>
+                <Picker.Item label="C°" value="metric" />
+                <Picker.Item label="F°" value="imperial" />
             </Picker>
         </View>
     )
+
 }
 const styles = StyleSheet.create({
-    unitSystem:{
-        position:'absolute',
-        top:100,
-        left:-50,
-        height:60,
-        width:100
-    }
+    unit: {
+        flex: 1,
+        marginLeft: 10
+    },
 })
